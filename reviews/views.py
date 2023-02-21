727e79c73d1f871daf08d43551e17d13de939aec
Kacper <kacper.mur123@gmail.com>
2023-02-21 18:32:06 +0100

    implemented basic views for index and add_review page

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(request):
+    return render(request, 'reviews/index.html')
+
+
+def add_review_view(request):
+    return render(request, 'reviews/add_review.html')
+
